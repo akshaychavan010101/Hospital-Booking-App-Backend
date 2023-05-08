1c5c40cf584f49cb5cff33978ea845567b751749
Akshay Chavan <115460540+akshaychavan010101@users.noreply.github.com>
2023-05-09 00:04:08 +0530

    Update users.routes.js

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -59,7 +59,7 @@ UserRouter.get("/google-verify", async (req, res) => {
  
     
     // redirect the user to the frontend
-    res.redirect("http://localhost:5173/");
+     res.redirect(`http://localhost:5173?token=${token}&username=${displayName}`);
   } catch (error) {
     console.log(error)
     res.status(500).json({ msg: "Something went wrong*" , error  });
