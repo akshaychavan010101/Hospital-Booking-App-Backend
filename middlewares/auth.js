3d3f978b74e143bae483a6edeac039299970ebeb
Akshay Chavan <115460540+akshaychavan010101@users.noreply.github.com>
2023-05-09 01:20:11 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 const auth = (passedRole) => {
   return (req, res, next) => {
     const { role } = req.user.dataValues;
-    console.log(req.user , role);
+    console.log(req.user , role , "*****");
     if (passedRole.includes(role)) {
       next();
     } else {
