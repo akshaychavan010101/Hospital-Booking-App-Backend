533ab7e30f5584b1202c6046f15cfff5fe6bee48
Akshay Chavan <115460540+akshaychavan010101@users.noreply.github.com>
2023-05-09 23:57:37 +0530

    Update users.routes.js

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -59,7 +59,7 @@ UserRouter.get("/google-verify", async (req, res) => {
  
     
     // redirect the user to the frontend
-     res.redirect(`http://localhost:5173?token=${token}&userName=${displayName}`);
+     res.redirect(`https://find-my-doctor.vercel.app?token=${token}&userName=${displayName}`);
   } catch (error) {
     console.log(error)
     res.status(500).json({ msg: "Something went wrong*" , error  });
